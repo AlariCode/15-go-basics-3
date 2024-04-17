f8f95dc021ced53e6527d70846bcd6a0d99f8f7d
AlariCode <antonlarichev@gmail.com>
2024-04-17 12:20:27 +0300

    Make для Map

--- main.go
+++ main.go
@@ -16,6 +16,11 @@ import "fmt"
 type bookmarkMap = map[string]string
 
 func main() {
+	m := make(bookmarkMap, 3)
+	m["A"] = "1"
+	m["B"] = "2"
+	m["C"] = "3"
+
 	bookmarks := bookmarkMap{}
 	fmt.Println("Приложение для закладок")
 Menu:
